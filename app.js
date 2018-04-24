8d2d9b67c84c4d3ddde83f6e8bb5a3e101125a7d
jwon114 <jthgwong@gmail.com>
2018-04-24 17:34:03 +1000

    materialize css styling index view, index view pagination

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var fetch = require('node-fetch');
+var _ = require('lodash');
 var app = express();
 
+// globals
 const PORT = 8080;
 const ticketsPerPage = 25;
 const baseURL = 'https://purpleduck.zendesk.com';
+var pageId;
+
 // functions for building API URL endpoints
 const pageTickets = (pageId) => `${baseURL}/api/v2/tickets.json?page=${pageId}&per_page=${ticketsPerPage}`;
 const showTicket = (id) => `${baseURL}/api/v2/tickets/${id}.json`;
-var pageId;
 
 // node settings and static content
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/node_modules/materialize-css/dist/'));
 
 function fetchData(url) {
   return (
@@ -27,6 +32,22 @@ function fetchData(url) {
   )
 }
 
+function calculatePages(total, perPage) {
+  if (totalTickets % ticketsPerPage === 0) {
+    return _.range(1, (totalTickets / ticketsPerPage) + 1);
+  } else { 
+    return _.range(1, Math.floor(totalTickets / ticketsPerPage) + 2);
+  }
+}
+
+function paginationIds(prevPage, nextPage, pageId) {
+  var previousId = null;
+  var nextId = null;
+  if (prevPage !== null) previousId = parseInt(pageId) - 1;
+  if (nextPage !== null) nextId = parseInt(pageId) + 1;
+  return [previousId, nextId];
+}
+
 app.get('/tickets/:id', function(req, res) {
   fetchData(showTicket(req.params.id))
   .then(data => {
@@ -38,7 +59,14 @@ app.get('/tickets', function(req, res) {
   req.query.page ? pageId = req.query.page : pageId = 1;
   fetchData(pageTickets(pageId))
   .then(data => {
-    res.render('pages/index', { tickets: data.tickets });
+    if (data.tickets.length !== 0) {
+      var totalTickets = data.count;
+      var totalPages = calculatePages(totalTickets, ticketsPerPage);
+      var [prevPageId, nextPageId] = paginationIds(data.previous_page, data.next_page, pageId);
+      res.render('pages/index', { tickets: data.tickets, totalPages, pageId, prevPageId, nextPageId, totalTickets });
+    } else {
+
+    }
   })
 });
 
